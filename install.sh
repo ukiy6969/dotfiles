bb1d4144a9076e504210e2844b495d14bba9bd14
ukiy6969 <ukiy6969@gmail.com>
2017-02-06 00:20:25 +0900

    :tada: pyenv

--- install.sh
+++ install.sh
@@ -8,3 +8,17 @@ do
 done
 
 ln -s $(realpath "${DIR}/config") "${HOME}/.config"
+
+pyenv install 2.7.11
+pyenv install 3.4.4
+
+pyenv virtualenv 2.7.11 neovim2
+pyenv virtualenv 3.4.4 neovim3
+
+pyenv activate neovim2
+pip install neovim
+pyenv which python  # Note the path
+
+pyenv activate neovim3
+pip install neovim
+pyenv which python  # Note the path
